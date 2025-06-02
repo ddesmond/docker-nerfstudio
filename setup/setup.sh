ee6d9abb6a50a160edcc02e1c2a54eafa47525de
Aleks <crni.aki@gmail.com>
2025-06-02 08:59:11 +0200

    update

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # start
-uv pip install nerfstudio
+uv pip install nerfstudio --system
 ns-install-cli
-uv pip install -e .[dev]
-uv pip install -e .[docs]
+uv pip install -e .[dev] --system
+uv pip install -e .[docs] --system
 ns-install-cli
 # end
 rm -rf /.nerfstudio-init
