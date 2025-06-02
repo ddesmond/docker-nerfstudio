e27d197172b74ec8dbba0ae4c76088fd0dba244c
Aleks <crni.aki@gmail.com>
2025-06-02 08:55:12 +0200

    update

--- setup/test.sh
+++ setup/test.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+cd /data && mkdir test && cd test
+# Download some test data:
+ns-download-data nerfstudio --capture-name=poster
+# Train model
+ns-train nerfacto --data data/nerfstudio/poster
