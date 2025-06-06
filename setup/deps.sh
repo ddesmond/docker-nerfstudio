c72ffe222b2b0a9d2ab7c4804086a1043d8c7315
Aleks <crni.aki@gmail.com>
2025-06-06 08:10:33 +0200

    update

--- setup/deps.sh
+++ setup/deps.sh
@@ -13,19 +13,8 @@ dnf install -y \
     nano zip \
     git git-lfs wget curl mlocate --allowerasing
 
-dnf install  -y \
-  make gcc patch zlib-devel bzip2 bzip2-devel \
-  readline-devel sqlite sqlite-devel openssl-devel \
-  tk-devel libffi-devel xz-devel libuuid-devel gdbm-libs libnsl2
-
-# add cuda nvcc
 dnf install -y \
-    mesa-libGLU \
-    libglvnd-glx
-
-##  cuda-nvcc-12* \
- # nvidia-cuda-toolkit \
- #
+    mesa-libGLU
 
 updatedb
 locate git | grep bin
